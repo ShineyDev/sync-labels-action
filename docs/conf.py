27b42fb779c219b63841bf0d372be630a7a9348f
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-07-01 12:32:05 +0100

    add documentation

---
+++ docs/conf.py
@@ -0,0 +1,55 @@
+import re
+
+
+extensions = [
+    "sphinx.ext.extlinks",
+    "sphinx_rtd_theme",
+]
+
+autodoc_member_order = "groupwise"
+autodoc_typehints = "none"
+
+extlinks = {
+    "issue": ("https://github.com/ShineyDev/sync-labels-action/issues/%s", "#"),
+}
+
+highlight_language = "yaml"
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    "collapse_navigation": False,
+    "includehidden": False,
+    "navigation_depth": -1,
+    "prev_next_buttons_location": None,
+    "titles_only": True,
+}
+master_doc = "index"
+pygments_style = "friendly"
+source_suffix = ".rst"
+
+copyright = "2021-present, ShineyDev"
+project = "sync-labels-action"
+
+_version_regex = r"^version = ('|\")((?:[0-9]+\.)*[0-9]+(?:\.?([a-z]+)(?:\.?[0-9])?)?)\1$"
+
+with open("../script.py") as stream:
+    match = re.search(_version_regex, stream.read(), re.MULTILINE)
+
+version = match.group(2)
+
+if match.group(3) is not None:
+    try:
+        import subprocess
+
+        process = subprocess.Popen(["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            version += out.decode("utf-8").strip()
+
+        process = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            version += "+g" + out.decode("utf-8").strip()
+    except (Exception) as e:
+        pass
+
+release = version
---
+++ docs/index.rst
@@ -0,0 +1,16 @@
+sync-labels-action
+==================
+
+A GitHub Action for synchronizing your GitHub repository labels with a labels.yml file.
+
+
+.. toctree::
+    :maxdepth: 2
+
+    introduction
+
+
+Still can't find what you're looking for?
+
+* :ref:`genindex`
+* :ref:`search`
