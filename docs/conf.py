0e01392e18f791dac0f74608dafd2016d30b9c91
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-11-08 16:33:20 +0000

    update documentation configuration

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,36 @@
 import re
 
 
+author = "ShineyDev"
+project = "sync-labels-action"
+
+copyright = f"2021-present, {author}"
+
+_version_regex = r"^version = ('|\")((?:[0-9]+\.)*[0-9]+(?:\.?([a-z]+)(?:\.?[0-9])?)?)\1$"
+
+with open("../script.py") as stream:
+    match = re.search(_version_regex, stream.read(), re.MULTILINE)
+
+release = "v" + match.group(2)
+version = release
+
+if match.group(3) is not None:
+    try:
+        import subprocess
+
+        process = subprocess.Popen(["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            release += out.decode("utf-8").strip()
+
+        process = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            release += "+g" + out.decode("utf-8").strip()
+    except (Exception) as e:
+        pass
+
+
 extensions = [
     "sphinx.ext.extlinks",
     "sphinx_rtd_theme",
@@ -10,10 +40,16 @@ autodoc_member_order = "groupwise"
 autodoc_typehints = "none"
 
 extlinks = {
-    "issue": ("https://github.com/ShineyDev/sync-labels-action/issues/%s", "#"),
+    "issue": (f"https://github.com/{author}/{project}/issues/%s", "#"),
 }
 
+
 highlight_language = "none"
+pygments_style = "friendly"
+root_doc = "index"
+source_suffix = ".rst"
+
+
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     "collapse_navigation": False,
@@ -22,34 +58,4 @@ html_theme_options = {
     "prev_next_buttons_location": None,
     "titles_only": True,
 }
-master_doc = "index"
-pygments_style = "friendly"
-source_suffix = ".rst"
-
-copyright = "2021-present, ShineyDev"
-project = "sync-labels-action"
-
-_version_regex = r"^version = ('|\")((?:[0-9]+\.)*[0-9]+(?:\.?([a-z]+)(?:\.?[0-9])?)?)\1$"
-
-with open("../script.py") as stream:
-    match = re.search(_version_regex, stream.read(), re.MULTILINE)
-
-version = match.group(2)
-
-if match.group(3) is not None:
-    try:
-        import subprocess
-
-        process = subprocess.Popen(["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE)
-        out, _ = process.communicate()
-        if out:
-            version += out.decode("utf-8").strip()
-
-        process = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE)
-        out, _ = process.communicate()
-        if out:
-            version += "+g" + out.decode("utf-8").strip()
-    except (Exception) as e:
-        pass
-
-release = version
+html_title = f"{project} {version}"
